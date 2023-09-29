71bc59eaa7b65203e4e987096c65489f85b4d2aa
Elias Gajo <frisoulion@hotmail.com>
2023-09-29 17:28:34 +0200

    QR14 part1

--- script.js
+++ script.js
@@ -19,3 +19,51 @@ function selectCurrentInputOnly(elem) {
 function getQcmContainer(elem) {
 	return elem.closest(".qr_question_qcm_container");
 }
+
+function drawTextBloc(text, x, y, dx, dy, fontSize, colorRect) {
+	var ctx = document.getElementById("schema").getContext("2d");
+	ctx.font = fontSize + "px serif";
+	ctx.textAlign="center";
+	ctx.textBaseline = "middle";
+	ctx.fillStyle= colorRect;
+	ctx.fillRect(x, y, dx, dy);
+	ctx.fillStyle= "black";
+	ctx.fillText(text, x + dx / 2, y + dy / 2);
+}
+function drawArrow(fromx, fromy, tox, toy, arrowWidth, color){
+	var ctx = document.getElementById("schema").getContext("2d");
+	var headlen = 10;
+	var angle = Math.atan2(toy-fromy,tox-fromx);
+ 
+	ctx.save();
+	ctx.strokeStyle = color;
+ 
+	//starting path of the arrow from the start square to the end square
+	//and drawing the stroke
+	ctx.beginPath();
+	ctx.moveTo(fromx, fromy);
+	ctx.lineTo(tox, toy);
+	ctx.lineWidth = arrowWidth;
+	ctx.stroke();
+ 
+	//starting a new path from the head of the arrow to one of the sides of
+	//the point
+	ctx.beginPath();
+	ctx.moveTo(tox, toy);
+	ctx.lineTo(tox-headlen*Math.cos(angle-Math.PI/7),
+			   toy-headlen*Math.sin(angle-Math.PI/7));
+ 
+	//path from the side point of the arrow, to the other side point
+	ctx.lineTo(tox-headlen*Math.cos(angle+Math.PI/7),
+			   toy-headlen*Math.sin(angle+Math.PI/7));
+ 
+	//path from the side point back to the tip of the arrow, and then
+	//again to the opposite side point
+	ctx.lineTo(tox, toy);
+	ctx.lineTo(tox-headlen*Math.cos(angle-Math.PI/7),
+			   toy-headlen*Math.sin(angle-Math.PI/7));
+ 
+	//draws the paths created above
+	ctx.stroke();
+	ctx.restore();
+}
