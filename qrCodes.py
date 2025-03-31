192e0b5cee0e200061cc659c26a431d5e1e3fb4e
Elias Gajo <frisoulion@hotmail.com>
2025-03-31 08:13:42 +0200

    40ème QR

--- qrCodes.py
+++ qrCodes.py
@@ -1,5 +1,5 @@
 import qrcode
 from PIL import Image
-for i in range(30):
+for i in range(40):
     qr = qrcode.make("https://mille-et-une-vignes.github.io/milleVignes/QR/" + str(i + 1) + ".html")
-    qr.save("C:/Users/friso/Documents/milleVignes/QRCodes/QR" + str(i + 1) + ".png");
+    qr.save("C:/Users/elias.gajo/Documents/milleVignes/QRCodes/QR" + str(i + 1) + ".png");
